783759f2eebd2f44675aa9de19cd4650a67a5fff
osman <hasanoguzhandemir@gmail.com>
2021-02-02 15:31:05 +0300

    Add adding score feature

--- business/business.go
+++ business/business.go
@@ -17,7 +17,178 @@ func NewBusiness(handler database.IDatabase) *Business {
 }
 
 func (b *Business) AddScore(score models.ScoreDTO) error {
-	return nil
+	user, err := b.handler.GetUserById(score.ID)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	root, err := b.handler.GetRoot()
+	if err != nil {
+		log.Println("could not get root ", err)
+		return err
+	}
+	if root.ID == user.ID {
+		if root.RightID == "" {
+			err = b.handler.SetRoot(root.LeftID)
+			if err != nil {
+				return err
+			}
+		} else if root.LeftID == "" {
+			err = b.handler.SetRoot(root.RightID)
+			if err != nil {
+				return err
+			}
+		} else {
+			err = b.handler.SetRoot(root.RightID)
+			if err != nil {
+				return err
+			}
+			userRight, err := b.handler.GetUserById(root.RightID)
+			if err != nil {
+				return err
+			}
+			left := userRight.LeftID
+			userRight.LeftID = root.LeftID
+			err = b.handler.UpdateUser(userRight)
+			if err != nil {
+				log.Println("could not update user ", err)
+				return err
+			}
+
+			leftUser, err := b.handler.GetUserById(left)
+			if err != nil {
+				return err
+			}
+
+			rootsLeft, err := b.handler.GetUserById(root.LeftID)
+			if err != nil {
+				return err
+			}
+
+			count, err := b.countNumberOfNodes(leftUser)
+			if err != nil {
+				return err
+			}
+
+			for rootsLeft.RightID != "" {
+				rootsLeft.RightCount += count
+				err = b.handler.UpdateUser(rootsLeft)
+				if err != nil {
+					return err
+				}
+				rootsLeft, err = b.handler.GetUserById(rootsLeft.RightID)
+				if err != nil {
+					return err
+				}
+			}
+			rootsLeft.RightCount += count
+			rootsLeft.RightID = left
+			err = b.handler.UpdateUser(rootsLeft)
+			if err != nil {
+				return err
+			}
+		}
+	} else {
+		parent, isLeftChild, err := b.findParentUser(user)
+		if err != nil {
+			return err
+		}
+		if user.RightID == "" {
+			if isLeftChild {
+				parent.LeftID = user.LeftID
+			} else {
+				parent.RightID = user.LeftID
+				parent.RightCount--
+			}
+			err = b.handler.UpdateUser(parent)
+			if err != nil {
+				return err
+			}
+		} else if user.LeftID == "" {
+			if isLeftChild {
+				parent.LeftID = user.RightID
+			} else {
+				parent.RightID = user.RightID
+				parent.RightCount--
+			}
+			err = b.handler.UpdateUser(parent)
+			if err != nil {
+				return err
+			}
+		} else {
+			//
+		}
+	}
+
+	err = b.handler.RemoveUserById(user.ID)
+	if err != nil {
+		return err
+	}
+	return b.CreateUser(models.UserDTO{
+		ID:          user.ID,
+		Score:       score.Score,
+		Name:        user.Name,
+		CountryCode: user.CountryCode,
+	})
+}
+
+func (b *Business) countNumberOfNodes(root *database.UserDAO) (int, error) {
+	if root == nil || root.ID == "" {
+		return 0, nil
+	}
+
+	var rightUser *database.UserDAO
+	var err error
+	if root.RightID != "" {
+		rightUser, err = b.handler.GetUserById(root.RightID)
+		if err != nil {
+			return 0, err
+		}
+	}
+	right, err := b.countNumberOfNodes(rightUser)
+	if err != nil {
+		return 0, err
+	}
+
+	var leftUser *database.UserDAO
+	if root.LeftID != "" {
+		leftUser, err = b.handler.GetUserById(root.LeftID)
+		if err != nil {
+			return 0, err
+		}
+	}
+	left, err := b.countNumberOfNodes(leftUser)
+	if err != nil {
+		return 0, err
+	}
+
+	return right + left + 1, nil
+}
+
+func (b *Business) findParentUser(user *database.UserDAO) (*database.UserDAO, bool, error) { //bool is to determine if it is left or right of parent. return true if it is left
+	root, err := b.handler.GetRoot()
+	if err != nil {
+		return nil, false, err
+	}
+	p := root
+	for p != nil {
+		if p.RightID == user.ID || p.LeftID == user.ID {
+			return p, p.LeftID == user.ID, nil //return true if it is left
+		}
+		if user.Score >= p.Score {
+			p, err = b.handler.GetUserById(p.RightID)
+			if err != nil {
+				return nil, false, err
+			}
+		} else {
+			p, err = b.handler.GetUserById(p.LeftID)
+			if err != nil {
+				return nil, false, err
+			}
+		}
+	}
+	return nil, false, errors.New("could not find parent user")
 }
 
 func (b *Business) CreateUser(user models.UserDTO) error {
